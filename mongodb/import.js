869eea557213ef210d9c7fbd0985141fe724b74b
nicolas <nicolas.hebrard@outlook.com>
2017-01-18 14:18:23 +0100

    Moulinette d'import

--- mongodb/import.js
+++ mongodb/import.js
@@ -1,26 +1,54 @@
-var mongodb = require('mongodb');
-var csv = require('csv-parser');
-var fs = require('fs');
+const mongodb = require('mongodb');
+const csv = require('csv-parser');
+const fs = require('fs');
 
-var MongoClient = mongodb.MongoClient;
-var mongoUrl = 'mongodb://localhost:27017/911-calls';
+const MongoClient = mongodb.MongoClient;
+const mongoUrl = 'mongodb://localhost:27017/911-calls';
 
-var insertCalls = function(db, callback) {
-    var collection = db.collection('calls');
+const CallCategory = {
+    EMS: 'EMS',
+    Fire: 'Fire',
+    Traffic: 'Traffic'
+};
 
-    var calls = [];
+const findCallCategory = (title) => {
+    if (title.startsWith(CallCategory.EMS)) {
+        return CallCategory.EMS;
+    } else if (title.startsWith(CallCategory.Fire)) {
+        return CallCategory.Fire;
+    } else if (title.startsWith(CallCategory.Traffic)) {
+        return CallCategory.Traffic;
+    } else {
+        return null;
+    }
+};
+
+const insertCalls = function (db, callback) {
+    const collection = db.collection('calls');
+
+    const calls = [];
     fs.createReadStream('../911.csv')
         .pipe(csv())
         .on('data', data => {
-            var call : {}; // TODO créer l'objet call à partir de la ligne
+            const call = {
+                latitude: parseInt(data.lat),
+                longitude: parseInt(data.lng),
+                description: data.desc,
+                zipCode: data.zip,
+                category: findCallCategory(data.title),
+                title: data.title,
+                timestamp: data.timeStamp,
+                area: data.twp,
+                address: data.addr
+            };
             calls.push(call);
         })
         .on('end', () => {
-          collection.insertMany(calls, (err, result) => {
-            callback(result)
-          });
+            collection.insertMany(calls, (err, result) => {
+                callback(result)
+            });
         });
-}
+};
 
 MongoClient.connect(mongoUrl, (err, db) => {
     insertCalls(db, result => {
