bb6bd541686075330ef34aaada720299d9053971
kedriss <cedric.linque@gmail.com>
2017-01-18 16:49:46 +0100

    implémentation de l'import de données dans elasticSearch

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,39 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+var i = 0;
+var calls=[];
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
       // TODO extract one line from CSV
+if (i++==0)console.log(data.lat);
+ 	calls.push({ "index" : { "_index" : "calls", "_type" : "data" } });
+
+	var i_call=
+		{
+		  lat: data.lat,
+  		  lng: data.lng,
+   		  desc: data.desc,
+  		  zip: data.zip,
+  		  title: data.title,
+		  type: data.title?data.title.split(':')[0]:"UNDEFINED",
+  		  timeStamp: data.timeStamp,
+		  month: data.timeStamp?data.timeStamp.split('-')[1]+'/'+data.timeStamp.split('-')[0]:"01/0000",
+  		  twp: data.twp,
+              	  addr: data.addr,
+  		  e:data.e
+		};
+	calls.push(i_call);
     })
     .on('end', () => {
       // TODO insert data to ES
+
+	esClient.bulk({ 
+			  body: calls
+			},function(err,resp,status) {
+			    console.log(resp);
+			});
+
+
     });
